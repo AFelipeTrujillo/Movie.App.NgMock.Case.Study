683148e36bd6a52bbf64bc3346d266445adf99d9
Andres Felipe Trujillo <andres.trujillo.1@gmail.com>
2016-10-24 09:02:11 -0500

    Testing HTTP With Mock

--- spec/movie-core/service.spec.js
+++ spec/movie-core/service.spec.js
@@ -13,14 +13,109 @@ describe('MovieCore', function(){
     //If any of the requests were not made, verifyNoOutstandingExpectation throws an exception.
     afterEach(function(){
         $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
     });
     
-    it('shoul create popular movie', function(){
+    it('should create popular movie', function(){
+        
+        /*var expectedData = function(data){
+            //dump(angular.mock.dump(data))
+            return angular.fromJson(data).movieId === 'tt0076759';
+        }*/
+        
+        // var expectedData = {"movieId":"tt0076759","description":"Greate Movie !"};
+        //Regular expression
+        var expectedData = /{"movieId":"tt0076759","description":".*"}/;
+        
+        $httpBackend.expectPOST(/./, expectedData)
+        .respond(201);
+        
         var popularMovie = new PopularMovies({
             movieId : 'tt0076759',
-            description : 'Greate Movie !'
+            description : 'Greate Movie !!!!!'
         });
         
         popularMovie.$save();
-    })
+        
+        expect($httpBackend.flush).not.toThrow();
+    });
+    
+    it('should get popular movie by id', function(){
+        $httpBackend.expectGET(function(url){
+            //dump(url);
+            return true;
+        }).respond(200);
+        
+        PopularMovies.get({ movieId : 'tt0076759'});
+        
+        expect($httpBackend.flush).not.toThrow();
+    });
+    
+    it('should update popular movie',function(){
+        $httpBackend.expectPUT()
+        .respond(200);
+        
+        var popularMovie = new PopularMovies({
+            movieId : 'tt0076759',
+            description : 'Greate Movie !!!!!'
+        });
+        
+        popularMovie.$update();
+        
+        expect($httpBackend.flush).not.toThrow();
+    });
+    
+    it('should authenticate requests',function(){
+        
+        /*var expectedHeaders = function(headers){
+            //dump(angular.mock.dump(headers));
+            return angular.fromJson(headers).authToken === 'teddybear';
+        }*/
+        
+        var expectedHeaders = {
+            "authToken": "teddybear",
+            "Accept": "application/json, text/plain, */*"
+        }        
+        
+        $httpBackend.expectGET('popular/tt0076759', expectedHeaders)
+        .respond(200);
+        
+        PopularMovies.get({ movieId : 'tt0076759'});
+        
+        expect($httpBackend.flush).not.toThrow();
+    });
+    
+    it('should authenticate requests completed way', function(){
+        
+        var headersData = function(headers){
+            return headers.authToken === 'teddybear';
+        }
+        
+        var matchAny = /.*/;
+        
+        $httpBackend.whenGET(matchAny, headersData)
+        .respond(200);
+        
+        $httpBackend.expectPOST(matchAny, matchAny ,headersData)
+        .respond(200);
+        
+        $httpBackend.expectPUT(matchAny, matchAny, headersData)
+        .respond(200);
+        
+        $httpBackend.expectDELETE(matchAny, headersData)
+        .respond(200);
+        
+        var popularMovie = new PopularMovies({
+            movieId : 'tt0076759',
+            description : 'Greate Movie !!!!!'
+        });
+        
+        PopularMovies.query();
+        PopularMovies.get({ movieId : 'tt0076759'})
+        new PopularMovies(popularMovie).$save();
+        new PopularMovies(popularMovie).$update();
+        new PopularMovies(popularMovie).$remove();
+        
+        expect($httpBackend.flush).not.toThrow();
+    });
 })
