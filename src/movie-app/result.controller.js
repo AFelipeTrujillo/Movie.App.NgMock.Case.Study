64b6172661c70a71a1c639a1820389530dbbe715
Andres Felipe Trujillo <andres.trujillo.1@gmail.com>
2016-11-02 13:10:13 -0500

    Ecceptions Testing

--- src/movie-app/result.controller.js
+++ src/movie-app/result.controller.js
@@ -1,5 +1,5 @@
 angular.module('movieApp')
-.controller('ResultController', function($scope, $location, omdbApi){
+.controller('ResultController', function($scope, $location, $exceptionHandler, omdbApi){
     $scope.results = [];
     //$scope.results.push({ data : { Title : 'Star Wars: Episode IV - A New Hope'} });
 
@@ -16,7 +16,8 @@ angular.module('movieApp')
     .then(function(data){
         $scope.results = data.Search;
     })
-    .catch(function(){
-        $scope.errorMessage = 'Something went wrong!';
-    })
+    .catch(function(e){
+      $exceptionHandler(e);
+      $exceptionHandler('other else !!');
+    });
 })
